1ca2a0321c8bb42743d863c73e303b2e2e1aafad
Cole Wilson <cole@colewilson.xyz>
2021-02-26 20:35:53 +0000

    added add item thing

--- helpers/handleadd.py
+++ helpers/handleadd.py
@@ -1,33 +1,82 @@
 import os
 import sys
 import mysql.connector
+import blessed
 
+term = blessed.Terminal()  # For colored output
 db = mysql.connector.connect(
-        username="pi",
-        password="robots",
-        database="replicator"
+	username="pi",
+	password="robots",
+	database="replicator"
 )
-
 cursor = db.cursor(buffered=True)
 
 def handlebarcode(itemname):
-    if itemname == '':
-        print('')
-        return
+	if itemname == '':
+		print('')
+		return
+	if len(itemname) > 44:
+		itemname = itemname[:44]
 
-    while True:
-        category = input('')
+	# Get Category ================
+	cursor.execute("SELECT * FROM categories")
+	validabrevs = []
+	for abbreviation, name in cursor.fetchall():
+		validabrevs.append(abbreviation)
+		print('{}: {}'.format(abbreviation, name))
+	while True:
+		itemcategory = input('Choose a category from the above list > ')
+		if itemcategory not in validabrevs:
+			print('That is not a valid option.')
+		else:
+			break
 
-    try:
-        sql = "SELECT item_name, qty_stock, qty_robot, qty_testing FROM items WHERE item_num = '{}'".format(itemname)
-        cursor.execute(sql)
-        name, stock, robot, testing = cursor.fetchall()[0]
-        db.commit()
-        print('(scan) moved one "{}" (barcode {}) from source {} to destination {}'\
-              '\t\tIn Stock: {}\t\tOn Robot: {}\t\tIn Testing: {}'.format(name, barcode, source, destination,
-                  stock, robot, testing
-              ))
-    except IndexError:
-        print(barcode, 'not found!')
-    except mysql.connector.Error as err:
-        pass
+	# Get Supplier ================
+	cursor.execute("SELECT * FROM categories")
+	validsuppliers = []
+	for abbreviation, name in cursor.fetchall():
+		validsuppliers.append(abbreviation)
+		print('{}: {}'.format(abbreviation, name))
+	while True:
+		itemsupplier = input('Choose a supplier from the above list > ')
+		if itemsupplier not in validsuppliers:
+			print('That is not a valid option.')
+		else:
+			break
+	# Get cost
+	while True:
+		itemcost = input('Cost of part > ')
+		try:
+			itemcost = float(itemcost)
+			break
+		else:
+			print('That is not a valid cost.')
+	# Get part number
+	partnum = input("Part Number >")
+	if len(partnum) > 12:
+		partnum = partnum[:12]
+	
+	# get qty
+	try:
+		qty_stock = int(input('Quantity in stock [blank for 0] > '))
+	except:
+		qty_stock = 0
+
+	try:
+		qty_robot = int(input('Quantity on robot [blank for 0] > '))
+	except:
+		qty_robot = 0
+
+	try:
+		qty_testing = int(input('Quantity in testing [blank for 0] > '))
+	except:
+		qty_testing = 0
+
+	try:
+		sql = "INSERT INTO items VALUES ('{}',NULL,'{}','{}','{}',{},{},{},{})".format(itemcategory,itemname,itemsupplier,partnum,itemcost,qty_stock,qty_robot,qty_testing)
+		cursor.execute(sql)
+		db.commit()
+		print("Added!")
+	except mysql.connector.Error as err:
+		pass
+	print()
