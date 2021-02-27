8134d50ae4c5365406aea6a0a10db66dc8c6d03c
Cole Wilson <cole@colewilson.xyz>
2021-02-27 02:40:23 +0000

    Update Image maker

--- helpers/OLED.py
+++ helpers/OLED.py
@@ -1,5 +1,14 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+import time
+
+font = ImageFont.load_default()
+
+padding = 10
 
 def OLED(source=None,destination=None):
-	img = Image.new('RGB', (100,100), 'red')
+	a = time.time()
+	img = Image.new('1', (128,64))
+	draw = ImageDraw.Draw(img)
+	draw.text((padding,padding), 'This is a test.', font=font, file=255)
 	img.save('test.png')
+	b = time.time()
