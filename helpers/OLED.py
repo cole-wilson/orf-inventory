4d123124bf255f6761506b3caa524fa11c86f4db
Cole Wilson <cole@colewilson.xyz>
2021-02-27 04:28:25 +0000

    Added image generation

--- helpers/OLED.py
+++ helpers/OLED.py
@@ -3,12 +3,25 @@ import time
 
 font = ImageFont.load_default()
 
-padding = 10
+padding = 8
 
-def OLED(source=None,destination=None):
+def OLED(source=None,destination=None,message=[],mode='dest',messagecolor='green'):
 	a = time.time()
-	img = Image.new('1', (128,64))
+	img = Image.open('logo.png')
 	draw = ImageDraw.Draw(img)
-	draw.text((padding,padding), 'This is a test.', font=font, file=255)
-	img.save('test.png')
+	if mode == 'src':
+		stext = "From: <- {} ->".format(source)
+		dtext = "To:      {}".format(destination)
+	else:
+		stext = "From:    {}".format(source)
+		dtext = "To:   <- {} ->".format(destination)
+	draw.text((padding,padding), stext, font=font, fill=(255,255,255))
+	draw.text((padding,padding + 10), dtext, font=font, fill=(255,255,255))
+	wrap = 13
+	for count, line in enumerate(message):
+		draw.text((padding,padding + 20 + (count*9)), line, font=font, fill=messagecolor)
+
+	img.save('output.png')
 	b = time.time()
+if __name__ == '__main__':
+	OLED(source='testsrc',destination='testdest',message=["","This is a","test message!"],mode='dest')
