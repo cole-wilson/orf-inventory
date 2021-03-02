4d9c6a9ceceba8e90f172f819c051ea60f3ad4fc
Cole Wilson <cole@colewilson.xyz>
2021-03-02 20:03:03 +0000

    changed to luma.oled

--- helpers/OLED.py
+++ helpers/OLED.py
@@ -1,16 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306 as ssd
+#import Adafruit_GPIO.SPI as SPI
+#import Adafruit_SSD1306 as ssd
+from luma.core.interface.serial import i2c
+from luma.oled.device import ssd1306, sh1106
 import subprocess
 import time
 
 cmode = '1'
 font = ImageFont.load_default()
-display = ssd.SSD1306_128_64(rst=0)
-display.begin()
+#display = ssd.SSD1306_128_64(rst=0)
+display = sh1106(i2c(port=1, address=0x3C))
+#display.begin()
 img = Image.open('logo.png').convert(cmode)
-display.image(img)
-display.display()
+display.display(img)
+#display.display()
 padding = 2
 fill = 255
 lastused = 0
@@ -24,9 +27,9 @@ def LOGO():
 	bg = Image.new('1',(128,64),'black')
 	img = Image.open('cropped-logo.png').convert(cmode).resize((64,64))
 	bg.paste(img,(32,0))
-	display.display()
-	display.image(bg)
-	display.display()
+	#display.display()
+	display.display(bg)
+	#display.display()
 
 def OLED(source=None,destination=None,message=[],mode='dest',messagecolor=255):
 	global lastused
@@ -47,9 +50,9 @@ def OLED(source=None,destination=None,message=[],mode='dest',messagecolor=255):
 	for count, line in enumerate(message):
 		draw.text((padding,padding + 20 + (count*9)), line, font=font, fill=messagecolor)
 	img.save('output.png')
-	display.display()
-	display.image(img)
-	display.display()
+	#display.display()
+	display.display(img)
+	#display.display()
 	lastused = time.time()
 
 OLED(source='stock',destination='robot',message=["","Welcome to the","inventory", "scanner!"],mode='dest')
